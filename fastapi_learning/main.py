4315899f16d7ca13d44f9035207a96857fb705ca
Chirag Solanki <solankichiragcs12@gmail.com>
2025-04-18 10:57:46 +0530

    add path parameters

--- fastapi_learning/main.py
+++ fastapi_learning/main.py
@@ -2,7 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+# @app.get('/')
 
-def say_hi():
-  return {"So we are in Github"}
+# def say_hi():
+#   return {"So we are in Github"}
+
+@app.get('/')
+def blog():
+  return  {'data' : {'Book':'Twilight book'}}
+
+@app.get('/blog/{id}')
+def showblog(id):
+    return {"This is for showing blog." : id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+   return {'data' : {'1','2'}}
+
+@app.get('/one/{id}')
+def change_type(id:int):
+   return {'data' :id}
